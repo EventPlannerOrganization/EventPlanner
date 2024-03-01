32d82b326c9e27e03ce89073a9b48ba25d64677f
MohammadShadid <3sfr3sfr@gmail.com>
2024-03-01 21:57:04 +0200

    adding some fields to User,RegisteredEvent Classes

--- src/main/java/models/RegisteredEvent.java
+++ src/main/java/models/RegisteredEvent.java
@@ -2,11 +2,15 @@ package models;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class RegisteredEvent {
     private String eventName;
+    private String location;
     private List<ServiceProvider> serviceProviders;
     private Date date;
+    private double cost;
+    private List<String> guestsEmails;
 
     public List<ServiceProvider> getServiceProviders() {
         return serviceProviders;
@@ -42,4 +46,43 @@ public class RegisteredEvent {
 
 
 
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+    public void addToCost(double added) {
+        cost += added;
+    }
+
+    public List<String> getGuestsEmails() {
+        return guestsEmails;
+    }
+
+    public void setGuestsEmails(List<String> guestsEmails) {
+        this.guestsEmails = guestsEmails;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegisteredEvent that = (RegisteredEvent) o;
+        return Double.compare(cost, that.cost) == 0 && Objects.equals(eventName, that.eventName) && Objects.equals(location, that.location) && Objects.equals(serviceProviders, that.serviceProviders) && Objects.equals(date, that.date) && Objects.equals(guestsEmails, that.guestsEmails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(eventName, location, serviceProviders, date, cost, guestsEmails);
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -4,17 +4,18 @@ import enumerations.UserType;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class User extends Person{
+public class User extends Person {
     private UserType usertype;
-    private List  <RegisteredEvent> registeredEvents;
+    private List<RegisteredEvent> registeredEvents;
+    private double totalCost;
 
-    public User(Name name, Authentication authentication, Address address, ContactInfo contactInfo ) {
+    public User(Name name, Authentication authentication, Address address, ContactInfo contactInfo) {
         super(name, authentication, address, contactInfo);
         usertype = UserType.USER;
-        registeredEvents=new ArrayList<>();
+        registeredEvents = new ArrayList<>();
     }
-
 
 
     public UserType getUsertype() {
@@ -29,7 +30,38 @@ public class User extends Person{
         return registeredEvents;
     }
 
-    public void setRegisteredEvent(List <RegisteredEvent> registeredEvent) {
+    public void setRegisteredEvent(List<RegisteredEvent> registeredEvent) {
+
         this.registeredEvents = registeredEvent;
     }
+
+    public List<RegisteredEvent> getRegisteredEvents() {
+        return registeredEvents;
+    }
+
+    public void setRegisteredEvents(List<RegisteredEvent> registeredEvents) {
+        this.registeredEvents = registeredEvents;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Double.compare(totalCost, user.totalCost) == 0 && usertype == user.usertype && Objects.equals(registeredEvents, user.registeredEvents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usertype, registeredEvents, totalCost);
+    }
 }
+
