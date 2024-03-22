ec565af753a959acd2ed65a6ad47b1822433650f
MohammadShadid <3sfr3sfr@gmail.com>
2024-03-22 21:02:31 +0200

    ff

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -15,3 +15,5 @@ X
 (src/main/java/printers/MenusPrinter.java,0\2\021c2be62f013ceace91f2c2ed3a341579499f30
 x
 Hsrc/test/java/testControllers/ServiceProvider/TestUpdateServiceType.java,f\8\f862f9579b9165e11a3dd4779eb54def8b25b2b8
+\
+,src/main/java/views/ServiceProviderView.java,9\c\9c2d0c476bea553419086711b2e1bac7f69b3822
--- src/main/java/launchers/Main.java
+++ src/main/java/launchers/Main.java
@@ -1,29 +1,18 @@
 package launchers;
 
-
-import Email.EmailService;
 import Exceptions.UserIsAlreadyExist;
 import Exceptions.WeakPasswordException;
 import models.EventPlanner;
 import views.StartingView;
-
 import javax.mail.MessagingException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
 public class Main {
 
     public static void main(String[] args) throws UserIsAlreadyExist, WeakPasswordException, IOException, MessagingException {
-
-
-
         EventPlanner.initializeRepositoryWithData();
         StartingView.staringView();
-
-
-
-
 
         }
 }
