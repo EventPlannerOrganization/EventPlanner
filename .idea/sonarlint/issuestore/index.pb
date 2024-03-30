8b3656e842b151af4105a600abbe3611a6debb8b
Baha-Alawneh <bahaalawneh07@gmail.com>
2024-03-31 01:15:51 +0300

    fix some code smells

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@ Y
 )src/main/java/models/RegisteredEvent.java,8\3\83659537ee34b1ed397e27924b7d620056e46354
 V
 &src/main/java/models/EventPlanner.java,5\d\5dfffa661285c32fbe8b07fa1ac73ede122045a6
+Q
+!src/main/java/launchers/Main.java,0\6\0629a6c541c440e68a61c01a03f3224f072556d3
--- src/main/java/launchers/Main.java
+++ src/main/java/launchers/Main.java
@@ -1,26 +1,16 @@
 package launchers;
 
-<<<<<<< HEAD
-import Exceptions.UserIsAlreadyExist;
-import Exceptions.UserNotFoundException;
-import Exceptions.WeakPasswordException;
-=======
 import exceptions.UserIsAlreadyExist;
 import exceptions.UserNotFoundException;
-import exceptions.WeakPasswordException;
->>>>>>> db351b393b6f9b887d29aba980598760b0183ecd
 import models.EventPlanner;
 import views.StartingView;
 import javax.mail.MessagingException;
 import java.io.IOException;
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> db351b393b6f9b887d29aba980598760b0183ecd
 public class Main {
-    public static void main(String[] args) throws UserIsAlreadyExist, WeakPasswordException, IOException, MessagingException, UserNotFoundException {
+    public static void main(String[] args) throws UserIsAlreadyExist, IOException, MessagingException, UserNotFoundException {
         EventPlanner.initializeRepositoryWithData();
         StartingView.staringView();
         }
